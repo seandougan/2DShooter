0ee25a19f923e0c358ff03e4ab80ab88cdeae6d9
Sean Dougan <mediauthority@gmail.com>
2017-10-21 15:40:25 -0400

    Scrolling BG
    (1)Scrolling background added

---
+++ 2D/Assets/_Script/BG_Scroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Scroll : MonoBehaviour {
+
+	//this varaible sets the speed at which the backrgound moves
+	public float scrollSpeed;
+
+	public float tileSize;
+
+	private Vector2 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSize);
+		transform.position = startPosition + Vector2.right * newPosition;
+	}
+}
